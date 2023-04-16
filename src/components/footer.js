066bd86f4efc226bac7fdc6d2c23d03517574f65
saqib <s248ali@uwaterloo.ca>
2023-04-16 15:18:25 -0400

    footer updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,38 +7,44 @@ function Footer() {
       <div class="footer">
         <div class="footer-content">
           <div class="footer-icons">
-            <Youtube
-              onClick={() => {
-                window.location.href =
-                  "https://www.youtube.com/channel/UChbV48W0We8mOFe50MJeKIQ";
-              }}
-              class="footer-icon"
-              size={30}
-              color={"white"}
-            />
-            <Github
-              onClick={() => {
-                window.location.href =
-                  "https://github.com/SaqibA1i?tab=repositories";
-              }}
-              class="footer-icon"
-              size={30}
-              color={"white"}
-            />
-            <Linkedin
-              onClick={() => {
-                window.location.href =
-                  "https://www.linkedin.com/in/saqib-ali-25b2aa213/";
-              }}
-              class="footer-icon"
-              size={30}
-              color={"white"}
-            />
+            <h3>Socials</h3>
+            <div classNam="footer-icon">
+              <Youtube
+                onClick={() => {
+                  window.location.href =
+                    "https://www.youtube.com/channel/UChbV48W0We8mOFe50MJeKIQ";
+                }}
+                size={30}
+                className="icon"
+                color={"white"}
+              />
+
+              <Github
+                onClick={() => {
+                  window.location.href =
+                    "https://github.com/SaqibA1i?tab=repositories";
+                }}
+                size={30}
+                color={"white"}
+              />
+              <Linkedin
+                onClick={() => {
+                  window.location.href =
+                    "https://www.linkedin.com/in/saqib-ali-25b2aa213/";
+                }}
+                size={30}
+                color={"white"}
+              />
+            </div>
+          </div>
+          <div class="footer-icons">
+            <h3>Contact & Info</h3>
+            <div classNam="footer-icon" style={{ textAlignLast: "start" }}>
+              <p>Saqib Ali</p>
+              <p>+1 647-561-7949</p>
+            </div>
           </div>
           <a href="mailto:saqibalip321@gmail.com">Lets talk!</a>
-          <div class="footer-bottom">
-            <p>Saqib Ali</p>
-          </div>
         </div>
       </div>
     </>
